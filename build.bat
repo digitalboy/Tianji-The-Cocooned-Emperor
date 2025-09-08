e01533ed68b4b657ae417172f04e58b6a9660ace
digitalboy <digitalboyzone@gmail.com>
2025-09-08 16:52:12 +0800

    modified:   build.bat

--- build.bat
+++ build.bat
@@ -1,6 +1,4 @@
 @echo off
-REM Set console to UTF-8 to handle Chinese filenames correctly.
-chcp 65001 > nul
 REM This script uses pandoc to convert the markdown chapters into an EPUB file.
 REM Make sure pandoc is installed and in your system's PATH.
 
